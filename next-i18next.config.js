d10a3cc0cfe04a01865b068ca8bd09967d53426e
Oleh <oleh.sirota@gmail.com>
2021-09-05 15:12:36 +0300

    localization

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'ua',
     locales: ['ua', 'ru'],
+    localePath: path.resolve('./public/locales'),
   },
   react: {
     useSuspense: false,
