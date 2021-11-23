990ae9d90b04ea18336f7daf0e5e68bfa17f27c5
oleh.sirota <oleh.sirota@gmail.com>
2021-11-23 12:18:40 +0200

    order

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -2,14 +2,14 @@ import dbConnect from 'utils/mongo';
 import Order from 'models/Order';
 
 export default async function handler(req: any, res: any) {
-  const { method } = req;
+  const { method, payload } = req;
   await dbConnect();
 
   switch (method) {
     case 'POST':
       try {
-        const data = await Order.create(req);
-        res.status(200).json({ req, data });
+        const data = await Order.create(payload);
+        res.status(200).json({ payload, data });
       } catch (error) {
         res.status(400).json(req);
       }
