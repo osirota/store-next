5a99a7038401bf6e281fb0818ec92e4eb84eea6c
oleh.sirota <oleh.sirota@gmail.com>
2021-11-23 11:57:43 +0200

    redesign

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -2,17 +2,16 @@ import dbConnect from 'utils/mongo';
 // import Order from 'models/Order';
 
 export default async function handler(req: any, res: any) {
-  const { method } = req;
+  const { method, response } = req;
   await dbConnect();
 
   switch (method) {
     case 'POST':
       try {
-        const response = req.body;
         // const data = await Order.create(response);
         res.status(200).json({ response });
       } catch (error) {
-        res.status(400).json(error);
+        res.status(400).json(response);
       }
       break;
     default:
