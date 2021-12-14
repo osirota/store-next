a946a04906b041ea74f2b400d96aacb3f8e736fa
oleh.sirota <oleh.sirota@gmail.com>
2021-12-14 22:10:59 +0200

    updates

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import { appWithTranslation } from 'next-i18next';
 import { RecoilRoot } from 'recoil';
 import SwiperCore, { EffectFade, Autoplay, Navigation } from 'swiper/core';
 import AppComponents from 'components/AppComponents';
+import * as gtag from 'utils/gtag';
 
 import 'swiper/swiper.min.css';
 import 'swiper/components/pagination/pagination.min.css';
@@ -16,7 +19,18 @@ import nextI18NextConfig from '../next-i18next.config';
 SwiperCore.use([EffectFade, Autoplay, Navigation]);
 
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
@@ -32,6 +46,24 @@ function MyApp({ Component, pageProps }) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <RecoilRoot>
         <AppComponents Component={Component} pageProps={pageProps} />
       </RecoilRoot>
